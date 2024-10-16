7d3498d430b7bd804b518460460f09cd60f942b2
Orlane Segolene Ketcha <orlanesegolene.ketcha@monboreal.ca>
2024-10-16 15:44:07 -0400

    Ajout du fichier AddPaiements.cs

---
+++ AppPaiements/Paiement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        public int Montant { get; set; }
+        public string Description { get; set; }
+        public Paiement(int montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+        }
+        public void AfficherDetails()
+        {
+            Console.WriteLine($"Montant : {Montant}, Description: {Description}");
+        }
+    }
+}
