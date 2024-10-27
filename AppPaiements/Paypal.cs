aff491dadbc0dd8339cc1d5c573f868c85286e34
Orlane Segolene Ketcha <orlanesegolene.ketcha@monboreal.ca>
2024-10-26 23:23:01 -0400

    Ajout du fichier Paypal.cs

---
+++ AppPaiements/Paypal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paypal : Paiement
+    {
+        // Propriétés Paypal
+        public string Courriel { get; set; }
+
+        // Constructeur
+        public Paypal (int montant, string description, string email) : base(montant, description)
+        {
+            Courriel = email;
+        }
+
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Montant: {Montant}, Description: {Description}, Email: {Courriel}");
+        }
+    }
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -4,9 +4,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+    // Instances de la classe CarteCredit
         CarteCredit CarteCredit1 = new CarteCredit(2500, "sac Hermes", 5058);
         CarteCredit CarteCredit2 = new CarteCredit(1600, "Vetements", 4724);
         CarteCredit CarteCredit3 = new CarteCredit(250, "Groceries", 5052);
+
+        // Instances de la classe Paypal
+        Paypal Paypal1 = new Paypal(700, "Loisir", "orlanesegolene@icloud.com");
+        Paypal Paypal2 = new Paypal(250, "Transport", "orlaneketcha64@gmail.com");
     }
 }
