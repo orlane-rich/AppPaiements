c63cebd613c7657478a5345a253f2ab72efcb75a
Orlane Segolene Ketcha <orlanesegolene.ketcha@monboreal.ca>
2024-10-26 23:49:15 -0400

    Ajout du fichier Utilisateur.cs

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+        public Utilisateur(string nom, List<Paiement> paiements)
+        {
+            Nom = nom;
+            Paiements = paiements;
+        }
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Nom : {Nom}, Paiements: {Paiements}");
+        }
+    }
+}
