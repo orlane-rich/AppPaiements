8303e6d6b21b65de7056a3ab005093faa6dadf08
Orlane Segolene Ketcha <orlanesegolene.ketcha@monboreal.ca>
2024-10-26 22:37:27 -0400

    Ajout du fichier cartecredit

---
+++ AppPaiements/CarteCredit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement
+    { //Proprietes
+        public int NumeroCarte {  get; set; }
+    // Constructeur
+    public CarteCredit (int montant, string description, int numeroCarte) : base (montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+        public void AfficherDetails() 
+        {
+            Console.WriteLine ($"Montant: {Montant}, Description: {Description}, NumeroCarte:{NumeroCarte}");
+        }
+    }
+}
